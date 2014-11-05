7116949ebd6873f13171cff5bd8b90a955b567ba
Johan Lindh <johan@linkdata.se>
2014-11-05 09:05:52 +0100

    added run-retail-servers.sh script

---
+++ nodejs-mariasql/run-retail-servers.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+nohup nodejs retail-demo-server.js 3000 &
+nohup nodejs retail-demo-server.js 3001 &
+nohup nodejs retail-demo-server.js 3002 &
+nohup nodejs retail-demo-server.js 3003 &
+nohup nodejs retail-demo-server.js 3004 &
+nohup nodejs retail-demo-server.js 3005 &
+nohup nodejs retail-demo-server.js 3006 &
+nohup nodejs retail-demo-server.js 3007 &
