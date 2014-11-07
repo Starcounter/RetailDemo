b822f24674b8cce8acf3598bdcb0744edf0ad190
Johan Lindh <johan@linkdata.se>
2014-11-07 21:48:30 +0100

    improve error reporting, add primary key to stats table

--- nodejs-mariasql/run-retail-servers.sh
+++ nodejs-mariasql/run-retail-servers.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-killall nodejs
+killall nodejs || true
 nohup nodejs retail-demo-server.js 3000 &
 nohup nodejs retail-demo-server.js 3001 &
 nohup nodejs retail-demo-server.js 3002 &
@@ -9,4 +9,4 @@ nohup nodejs retail-demo-server.js 3004 &
 nohup nodejs retail-demo-server.js 3005 &
 nohup nodejs retail-demo-server.js 3006 &
 nohup nodejs retail-demo-server.js 3007 &
-tail -f nohup.out
+top
